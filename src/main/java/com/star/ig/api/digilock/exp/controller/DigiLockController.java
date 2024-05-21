3987ed7934a8579f77b9fa7fdf8b69109268bda4
v.prithivirajt <v.prithivirajt@starhealth.in>
2024-05-21 19:08:09 +0530

    Devops trainig

--- src/main/java/com/star/ig/api/digilock/exp/controller/DigiLockController.java
+++ src/main/java/com/star/ig/api/digilock/exp/controller/DigiLockController.java
@@ -2,20 +2,17 @@ package com.star.ig.api.digilock.exp.controller;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Slf4j
+
 @RestController
 @RequestMapping("ig-digi-lock/api/v4/digi")
 @Api("DigiLock Services")
-@AllArgsConstructor(onConstructor_ = { @Autowired })
+
 @Validated
 public class DigiLockController {
 
